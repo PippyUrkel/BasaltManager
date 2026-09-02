9eee78b243fc26bd1a7d8a56b2686fb3f34b0c77
Andre Dsouza <andredsouza256@gmail.com>
2026-09-02 10:21:32 +0530

    docs(documentation): Initiated scaffold

--- android/app/src/main/kotlin/com/example/basalt_server_management/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.basalt_server_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/theme/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final ThemeData appTheme = ThemeData( 
-  colorScheme: ColorScheme.fromSeed( 
-    seedColor: const Color(0xFF43B5A0),
-    brightness: Brightness.dark,
-  ),  
-  useMaterial3: true,
-);
